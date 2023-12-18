429549b8ffb2271d4d3dc0d66b6601af7f30ba55
PIPEPIR <6322771633@g.siit.tu.ac.th>
2023-12-18 17:31:37 +0700

    fix: mock model prediction

--- script.js
+++ script.js
@@ -3,15 +3,15 @@ const mockData = {
   datasets: [
     {
       label: "Predicted",
-      data: [3600, 3200, 3500, 3200, 5500, 12900], // Rounded predicted data
+      data: [3600, 3200, 3500, 3200, 5500, 12900], 
       fill: false,
       borderColor: "orange",
       tension: 0.1,
-      borderDash: [5, 5] // Dashed line for predicted data
+      borderDash: [5, 5]
     },
     {
       label: "Actual",
-      data: [2500, 4000, 3000, 3100, 6000, 14000], // Rounded actual data
+      data: [2500, 4000, 3000, 3100, 6000, 14000], 
       fill: false,
       borderColor: "grey",
       tension: 0.1
@@ -20,7 +20,7 @@ const mockData = {
 };
 
 
-// Initialize the Chart.js graph
+
 const ctx = document.getElementById("resultsGraph").getContext("2d");
 const resultsGraph = new Chart(ctx, {
   type: "line",
@@ -37,46 +37,38 @@ const resultsGraph = new Chart(ctx, {
   },
 });
 
-// Modify the getMockPrediction function to use input values
+
 function getMockPrediction(rainfall, temperature) {
-  const predictionBase = mockData.datasets[1].data.slice(-1)[0];
-  const rainfallEffect = rainfall * 0.1;
-  const temperatureEffect = temperature * 0.2;
-  return predictionBase + rainfallEffect + temperatureEffect;
+
+  return  Math.random() * (14000 - 2000) + 2000;;
 }
 
-// Function to update the chart with a new predicted value for the next month
 function addCustomPredictionToChart(chart, prediction) {
-  // Determine the next month and date
   const lastLabel = chart.data.labels[chart.data.labels.length - 1];
-  const lastMonth = new Date(lastLabel + " 1, 2023"); // Year is arbitrary, added day for parsing
+  const lastMonth = new Date(lastLabel + " 1, 2023"); 
   const nextMonth = new Date(lastMonth.setMonth(lastMonth.getMonth() + 1));
   const nextMonthLabel = nextMonth.toLocaleString('en-us', { month: 'short' });
 
-  // Add the next month to the labels
   chart.data.labels.push(nextMonthLabel);
 
-  // Update the "Predicted" dataset
   chart.data.datasets.forEach((dataset) => {
     if (dataset.label === "Predicted") {
       dataset.data.push(prediction);
     } else {
-      // For other datasets (like "Actual"), push null or handle as needed
       dataset.data.push(null);
     }
   });
 
-  // Refresh the chart to show the new data
   chart.update();
 }
 
 
-// Event listener for the predict button
+
 document.getElementById("predictBtn").addEventListener("click", () => {
   const rainfall = parseFloat(document.getElementById("rainfall").value);
   const temperature = parseFloat(document.getElementById("temperature").value);
 
-  // Validate the input
+
   if (isNaN(rainfall) || isNaN(temperature)) {
     alert("Please enter valid numbers for rainfall and temperature.");
     return;
@@ -85,7 +77,7 @@ document.getElementById("predictBtn").addEventListener("click", () => {
   const predictedValue = getMockPrediction(rainfall, temperature);
   addCustomPredictionToChart(resultsGraph, predictedValue);
 
-  // Update the "Predicted Case" display
+ 
   document.getElementById("predictedCase").textContent = `${Math.round(predictedValue)} people`;
 
   document.getElementById("graphSection").style.display = "block";
